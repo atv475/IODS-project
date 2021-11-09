926574cab6f50a86f2f75a4a4162b09ec6f37b87
Ari Voutilainen <ariv@uef.fi>
2021-11-09 14:43:19 +0200

    Exploring variables in Exercise 2.

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -22,12 +22,14 @@ lrn14$stra <- rowMeans(strategic_columns)
 
 surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
 surface_columns <- select(lrn14, one_of(surface_questions))
+
 lrn14$surf <- rowMeans(surface_columns)
+lrn14$atti <- lrn14$Attitude / 10
 
 #I found the name of the one_of function misleading. It does not select just one but all.
 
 #Creating an analysis data set.
-keep_columns <- c("gender","Age","Attitude", "deep", "stra", "surf", "Points")
+keep_columns <- c("gender","Age","atti", "deep", "stra", "surf", "Points")
 learning2014 <- select(lrn14, one_of(keep_columns))
 learning2014 <- filter(learning2014, Points > 0)
 
@@ -42,6 +44,6 @@ getwd()
 #Saving the analysis data set.
 ?write.csv
 write.csv(learning2014, file = "learning2014.csv")
-read.csv
+read.csv("learning2014.csv")
 str(learning2014)
 head(learning2014)
