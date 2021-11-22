e477bea0e03d77bbf27bc2666d78aba63955eb30
Ari Voutilainen <ariv@uef.fi>
2021-11-22 15:08:13 +0200

    Ex 4: First steps of data wrangling.

---
+++ data/create_human.R
@@ -0,0 +1,46 @@
+#Ari Voutilainen
+#November 22, 2021
+#R script for data wrangling in IODS2021 Exercise 4.
+
+#Reading "Human development" (HD) and "Gender inequality" (GI) data sets into R.
+setwd("C:/Users/ariv/Documents/IODS-project/IODS-project/data")
+getwd()
+hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
+gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+
+#Exploring the HD data set.
+colnames(hd)
+str(hd)
+dim(hd)
+summary(hd)
+
+#Exploring the GI data set.
+colnames(gii)
+str(gii)
+dim(gii)
+summary(gii)
+
+#Renaming the HD variables.
+library(tidyverse)
+colnames(hd)
+rename(hd, HDIrank = HDI.Rank
+       , HDI = Human.Development.Index..HDI.
+       , LE = Life.Expectancy.at.Birth
+       , EDUCexp = Expected.Years.of.Education
+       , EDUCmean = Mean.Years.of.Education
+       , GNI = Gross.National.Income..GNI..per.Capita
+       , GNIHDI = GNI.per.Capita.Rank.Minus.HDI.Rank
+       )
+
+#Renaming the GI variables.
+colnames(gii)
+rename(gii, GIIrank = GIIrank
+       , GII = Gender.Inequality.Index..GII.
+       , MMR = Maternal.Mortality.Ratio
+       , ABR = Adolescent.Birth.Rate
+       , Parl = Percent.Representation.in.Parliament
+       , EDUCF = Population.with.Secondary.Education..Female.
+       , EDUCM = Population.with.Secondary.Education..Male.
+       , LABF = Labour.Force.Participation.Rate..Female.
+       , LABM = Labour.Force.Participation.Rate..Male.
+       )
